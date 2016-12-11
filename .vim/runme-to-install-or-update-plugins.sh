014ef96731d08a80e907b06fd01dc4adf19b7652
Maximilian Weiss <max@maxweiss.io>
2016-12-11 14:18:12 -0800

    Updated plugin installer shell script

---
+++ .vim/runme-to-install-or-update-plugins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Do not move this script, it must stay in the current directory!
+echo 'Re-installing Vundle'
+rm -rf $(pwd)/bundle/*
+git clone https://github.com/VundleVim/Vundle.vim.git $(pwd)/bundle/Vundle.vim > /dev/null 2>&1
+echo 'Finished'
+echo 'Re-installing Plugins'
+vim -i NONE -c PluginInstall -c quitall > /dev/null 2>&1
+echo 'Finished'
+echo 'Updating Plugins'
+vim -i NONE -c VundleUpdate -c quitall > /dev/null 2>&1
+echo 'Finished'
+echo 'Remaking YouCompleteMe objects'
+cd $(pwd)/bundle/YouCompleteMe
+./install.py --clang-completer > /dev/null 2>&1
+echo 'Finished'
+echo ''
+echo 'Script completed.'
+
--- .vim/runme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-vim -c PluginInstall -c quitall
-sleep 2
-vim -c VundleUpdate -c quitall
-sleep 2
-cd $(pwd)/bundle/YouCompleteMe
-./install.py --clang-completer
