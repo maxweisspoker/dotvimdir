bdc88f9c246f68330ce9ade650e18459a49cc8f6
Maximilian Weiss <max@maxweiss.io>
2017-01-02 23:21:40 -0800

    Added some java plugins, disabled YouCompleteMe updates

--- .vim/runme-to-install-or-update-plugins.sh
+++ .vim/runme-to-install-or-update-plugins.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Do not move this script, it must stay in the current directory!
 echo 'Re-installing Vundle'
+
+#######################
+mv $(pwd)/bundle/YouCompleteMe $(pwd)
+#######################
+
 rm -rf $(pwd)/bundle/*
 git clone https://github.com/VundleVim/Vundle.vim.git $(pwd)/bundle/Vundle.vim > /dev/null 2>&1
 echo 'Finished'
@@ -10,10 +15,15 @@ echo 'Finished'
 echo 'Updating Plugins'
 vim -i NONE -c VundleUpdate -c quitall > /dev/null 2>&1
 echo 'Finished'
-echo 'Remaking YouCompleteMe objects'
-cd $(pwd)/bundle/YouCompleteMe
-./install.py --clang-completer > /dev/null 2>&1
-echo 'Finished'
+
+#########################3
+#echo 'Remaking YouCompleteMe objects'
+#cd $(pwd)/bundle/YouCompleteMe
+#./install.py --clang-completer > /dev/null 2>&1
+#echo 'Finished'
+mv $(pwd)/YouCompleteMe $(pwd)/bundle/YouCompleteMe
+#############################3
+
 echo ''
 echo 'Script completed.'
 
