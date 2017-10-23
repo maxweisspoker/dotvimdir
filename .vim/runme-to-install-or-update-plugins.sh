4c794db53cc7606054f04e14efd9fd5354f4f40b
Maximilian Weiss <max@maxweiss.io>
2017-10-23 09:48:17 -0700

    Minor update, don't track bundle/

--- .vim/runme-to-install-or-update-plugins.sh
+++ .vim/runme-to-install-or-update-plugins.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Do not move this script, it must stay in the current directory!
 echo 'Re-installing Vundle'
-rm -rf $(pwd)/bundle/*
+rm -rf $(pwd)/bundle/
+mkdir $(pwd)/bundle/
 git clone https://github.com/VundleVim/Vundle.vim.git $(pwd)/bundle/Vundle.vim > /dev/null 2>&1
 echo 'Finished'
 echo 'Re-installing Plugins'
